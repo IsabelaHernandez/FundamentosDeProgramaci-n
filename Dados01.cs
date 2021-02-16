c54437ef93d1068e047865555735bf6b03eb1128
IsabelaHernandez <77949810+IsabelaHernandez@users.noreply.github.com>
2021-02-15 21:35:05 -0500

    Add files via upload

---
+++ Dados01.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Dados01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            string respuesta = "s";
+            int total = 0;
+            int dadoa = 0, dadob = 0;
+            int par = 0, totaltiros = 0;
+            double contador1 = 0, contador2 = 0; //Contador1 : total, Contador2: dadoa+dadob<6
+            
+
+            //Inicio
+
+            Console.WriteLine("Desea jugar? (s/n)");
+            respuesta = Console.ReadLine();
+
+
+            while (respuesta == "s")
+            {
+                contador1 += 1;
+
+                dadoa = aleatorio.Next(1, 7);
+                Console.Write("Dado a:" + dadoa);
+
+                dadob = aleatorio.Next(1, 7);
+                Console.WriteLine(" Dado b:" + dadob);
+
+                total = dadoa + dadob;
+                Console.Write("Total dados:" + total);
+
+                totaltiros += total;
+                Console.WriteLine(" Total tiros:" + totaltiros);
+
+                if (total > 6) contador2 += 1;
+                
+                if (dadoa == dadob) par += 1;
+                Console.WriteLine("Total de pares:" + par);
+
+                if ((dadoa == 1) && (dadob == 1))
+                {
+                    
+                    Console.WriteLine("Has perdido");
+                    respuesta = "n";
+
+                    double porcentaje = (contador2 / contador1) * 100;
+                    Console.WriteLine("Porcentaje de tiros mayores a 6:" + porcentaje);
+
+                }
+
+                if ((totaltiros > 99) || (par == 3))
+                {
+                    Console.WriteLine("Felicidades, has ganado.");
+                    respuesta = "n";
+
+                    double porcentaje = (contador2 / contador1) * 100;
+                    Console.WriteLine("Porcentaje de tiros mayores a 6:" + porcentaje);
+                }
+
+                if ((totaltiros <100) && (respuesta== "s"))
+                {
+                    Console.WriteLine("Desea continuar? (s/n)");
+                    respuesta = Console.ReadLine(); 
+                }
+                    
+
+                
+            }
+
+        }
+    } 
+}
+//https://youtu.be/ZYZwHCmkyuk
---
+++ ValoresExtremos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ValoresExtremos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            int i = 0; //Contador
+            int max = 0, min = 200;
+            string nombremin = "", nombremax = "";
+           
+
+            Console.WriteLine("Ingrese el número de datos (n)");
+            int n = int.Parse(Console.ReadLine());
+
+
+            while (i < n)
+            {
+                Console.Write("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                Console.Write("Nombre: ");
+                string nombre = Console.ReadLine();
+
+                if (edad < min)
+                {
+                    min = edad;
+                    nombremin = nombre;
+                }
+                if (edad > max)
+                {
+                    max = edad;
+                    nombremax = nombre;
+                }
+
+                total += edad;
+                i++;             
+                
+            }
+
+            double promedio = total / n;
+            Console.WriteLine("Promedio =" + promedio);
+            Console.WriteLine("Mayor =" + nombremax + " Edad=" + max );
+            Console.WriteLine("Menor =" + nombremin + " Edad=" + min);
+        } 
+    }
+}
+
+//https://youtu.be/ZYZwHCmkyuk
