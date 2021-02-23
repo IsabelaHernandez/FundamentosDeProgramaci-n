52bd5501323271a642ed5b168b0a6ca6301005de
IsabelaHernandez <77949810+IsabelaHernandez@users.noreply.github.com>
2021-02-22 22:13:11 -0500

    Add files via upload

---
+++ MetodosNumericos01.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MetodosNumericos01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                Console.WriteLine("Ingrese x: ");
+
+                double x = double.Parse(Console.ReadLine());
+
+                int n = 100;
+
+                double total = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    total += (Math.Pow(x, i) / Factorial(i));
+                }
+                Console.WriteLine("Valor de 'e' : " + total);
+            }
+            static double Factorial(double valor)
+            {
+                if (valor <= 1) return 1;
+                return valor * Factorial(valor - 1);
+               
+            }
+        }
+    }
+}
+
+//https://youtu.be/tcjG32-spH0
---
+++ MetodosNúmericos02.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MetodosNúmericos02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese x");
+           
+            double x = double.Parse(Console.ReadLine());
+
+            Sin(x);
+        }
+        
+        static void Sin(double x)
+        {
+            double resultado = 0;
+
+            int n = 100;
+           
+            for (int i = 0; i < n; i++)
+            {
+                resultado += ((Math.Pow(-1, i)) / Factorial((2 * i) + 1)) * (Math.Pow(x, (2 * i) + 1)); 
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+
+        }
+
+        static double Factorial(double valor)
+        {
+            if (valor <= 1) return 1;
+            return valor * Factorial(valor - 1);
+        }
+        
+
+}
+}
+
+//https://youtu.be/tcjG32-spH0
