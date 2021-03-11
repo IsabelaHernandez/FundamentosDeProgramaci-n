db6b6e1df9ffff1285f3c855c78b0e888a2d70bf
IsabelaHernandez <77949810+IsabelaHernandez@users.noreply.github.com>
2021-03-10 22:44:22 -0500

    Add files via upload

---
+++ Reto String Frase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Reto_Frases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = "", palabra = "";
+            int contador = 0;
+
+            Console.WriteLine("Ingrese la frase");
+            frase = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Ingrese la palabra en cuestión");
+            palabra = Console.ReadLine().ToUpper();
+
+            string[] palabras = frase.Split(" ");
+
+          
+            for (int i = 0; i < palabras.Length; i++)
+            {
+                if (palabras[i] == palabra)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Palabra en posición: " + i);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    contador++;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Total de veces que apareció la palabra: " + contador );
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
+//https://youtu.be/ApyF4rIoyCQ
